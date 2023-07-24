4b6fda5a458bbe326714176d9dee039ca3555ddd
Jhonleandres <jhonleandres@cessistemas.com.br>
2023-07-24 17:20:22 -0300

    dsadas

---
+++ Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using apiGestorWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiGestorWeb.Controllers.Tests
+{
+    [TestClass()]
+    public class WeatherForecastControllerTests
+    {
+        [TestMethod()]
+        public void WeatherForecastControllerTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
